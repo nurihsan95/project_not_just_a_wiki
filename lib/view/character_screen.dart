93944d361b34976a3e2faf8e411ae3e6a400d2e2
Nurul Ihsan Hariz Pratama <nurul.ihsanharizpratama@gmail.com>
2021-06-01 12:41:00 +0700

    Character screen list tile

--- lib/view/character_screen.dart
+++ lib/view/character_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hexcolor/hexcolor.dart';
+import 'package:project_not_just_a_wiki/component/constant.dart';
 
 class CharacterScreen extends StatefulWidget {
   static String screenId = 'character_screen';
@@ -28,7 +29,113 @@ class _CharacterScreenState extends State<CharacterScreen> {
         centerTitle: true,
       ),
       backgroundColor: HexColor('#ff17293A'),
-      body: SafeArea(),
+      body: SafeArea(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Container(
+              constraints: BoxConstraints(maxWidth: 100.0),
+              decoration: BoxDecoration(
+                  border: Border.all(color: HexColor('#FFFF4756'))),
+              child: Image.network(
+                  'https://static.wikia.nocookie.net/valorant/images/1/14/Phoenix_icon.png/revision/latest?cb=20201128234131'),
+            ),
+            Expanded(
+              child: Container(
+                constraints: BoxConstraints(maxHeight: 50.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: <Widget>[
+                    Text(
+                      //Agent name
+                      'Phoenix',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 25.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Column(
+                      //Role
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: <Widget>[
+                        Container(
+                          constraints:
+                              BoxConstraints(maxWidth: 20.0, maxHeight: 20.0),
+                          child: Image.network(duelistNetworkIcon),
+                        ),
+                        Text(
+                          'Duelist',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Column(
+                      //Tier
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: <Widget>[
+                        Text(
+                          'A',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20.0,
+                          ),
+                        ),
+                        Text(
+                          'Tier',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Expanded(
+                      child: Column(
+                        //Difficulty
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget>[
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Icon(
+                                Icons.circle_rounded,
+                                color: Colors.white,
+                                size: 20.0,
+                              ),
+                              Icon(
+                                Icons.circle_rounded,
+                                color: Colors.white,
+                                size: 20.0,
+                              ),
+                              Icon(
+                                Icons.circle_outlined,
+                                color: Colors.white,
+                                size: 20.0,
+                              ),
+                            ],
+                          ),
+                          Text(
+                            'Average',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
